87f562315b175616670134bab9f41b3a463746cf
clmcdonald9 <clmcdonald9@alaska.edu>
2026-09-24 14:56:09 -0800

    course name now has a state

--- src/components/courses/CourseForm.jsx
+++ src/components/courses/CourseForm.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 
 function CourseForm() {
+    const [courseName, setCourseName] = useState("");
+
     return (
         <div>
             <h1>New Course</h1>
             <form>
                 <label>Course Name:
-                    <input type ="text" placeholder="Course Name" />
+                    <input type ="text" placeholder="Course Name" onChange={(e) => setCourseName(e.target.value)}/>
                 </label>
+                <button type="submit">Submit</button>
+                <p>Course Name: {courseName}</p>
             </form>
         </div>
     );
