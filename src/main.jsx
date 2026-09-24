0b62fe357339b8a85bb38a8d6607794d6731a4dc
David Gray <davidagray2005@gmail.com>
2026-09-23 21:30:46 -0800

    added a dashboard component that im testing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import DashboardPanel from './components/dashboard/DashboardPanel.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <DashboardPanel />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
