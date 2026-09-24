8969f4ae4c8a0c50d294737fb416a12c51566d1e
clmcdonald9 <clmcdonald9@alaska.edu>
2026-09-24 14:01:38 -0800

    basic form displays on page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import CourseForm from "./components/courses/CourseForm";
+
 function App() {
   return (
     <main className="min-h-screen bg-slate-950 text-white">
       <h1 className="text-3xl font-bold">Student Productivity Dashboard</h1>
+      <CourseForm />
     </main>
   );
 }
---
+++ src/components/courses/CourseForm.jsx
@@ -0,0 +1,15 @@
+
+function CourseForm() {
+    return (
+        <div>
+            <h1>New Course</h1>
+            <form>
+                <label>Course Name:
+                    <input type ="text" placeholder="Course Name" />
+                </label>
+            </form>
+        </div>
+    );
+}
+
+export default CourseForm;
