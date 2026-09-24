d407c77b5cfa455c4eefa837e5242f269abb5e90
David Gray <davidagray2005@gmail.com>
2026-09-23 21:31:07 -0800

    Creating the dashboard panel component

---
+++ src/components/dashboard/DashboardPanel.jsx
@@ -0,0 +1,9 @@
+function DashboardPanel({ title, subtitle, children, className = '' }) {
+  return (
+    <section className={`border rounded-lg p-5 bg-zinc-900 border-zinc-800 ${className}`}>
+      <h1>"test</h1>
+    </section>
+  );
+}
+
+export default DashboardPanel;
