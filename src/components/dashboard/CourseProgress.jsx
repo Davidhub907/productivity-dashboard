791e6228a1305a7fb7b33f6ab56fa21f7dca4016
honey <crystadarling@yahoo.com>
2026-09-25 19:25:22 -0800

    course form popup window appears

--- src/components/dashboard/CourseProgress.jsx
+++ src/components/dashboard/CourseProgress.jsx
@@ -1,14 +1,25 @@
 import {useState} from 'react';
 import DashboardPanel from './DashboardPanel';
+import CourseForm from '../courses/CourseForm'
 
 function CourseProgress() {
     const [showAddCourseForm, setShowAddCourseForm] = useState(false);
 
     return(
-        <DashboardPanel title="COURSE PROGRESS" subtitle="Fall Sem" className="min-h-[400px]" onHeaderAction={() => console.log('Header action clicked!')}>
-            <p>Course progress placeholder</p>
-        </DashboardPanel>
-    )
+        <>
+            <DashboardPanel title="COURSE PROGRESS" subtitle="Fall Sem" className="min-h-[400px]" onHeaderAction={() => setShowAddCourseForm(!showAddCourseForm)}>
+                <p>Course progress placeholder</p>
+            </DashboardPanel>
+
+            {showAddCourseForm && (
+                <div className="fixed inset-0 flex items-center justify-center bg-black bg-black/60">
+                    <div className="rounded-lg border-2 border-zinc-800 bg-zinc-900 p-6 text-white">
+                        <CourseForm />
+                    </div>
+                </div>
+            )}
+        </>
+    );
 }
 
 export default CourseProgress;
